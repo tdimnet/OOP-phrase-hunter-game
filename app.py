0f239239cd01c090242873e828cfc1da944a7867
Thomas Dimnet <thomas.dimnet@gmail.com>
2021-04-06 17:43:20 +0200

    Game starts taking shape

--- app.py
+++ app.py
@@ -1,32 +1,28 @@
 import random
 
 PHRASES = [
-    "May the Force be with you.",
-    "Elementary, my dear Watson.",
-    "My mama always said life was like a box of chocolates.",
+    # "May the Force be with you.",
+    # "Elementary, my dear Watson.",
+    # "My mama always said life was like a box of chocolates.",
     "My precious.",
-    "Bond. James Bond."
+    # "Bond. James Bond."
 ]
 
 
-def display_phrase(phrase, test_letter):
-    print(list(phrase))
-    print(test_letter)
-
+def build_placeholder(phrase, test_letter):
     placeholder = []
 
     for letter in phrase:
         if letter.isalpha():
-            if letter.lower() == test_letter.lower():
+            if letter.lower() in test_letter:
                 placeholder.append(letter)
             else:
                 placeholder.append("_")
         else:
             placeholder.append(letter)
 
-    print("====")
-    print("".join(placeholder))
-    print("====")
+
+    return "".join(placeholder)
 
 
 def choose_random_phrase():
@@ -34,12 +30,21 @@ def choose_random_phrase():
 
 
 def main():
+    is_playing = True
+    number_of_attempts = 0
+
     phrase = choose_random_phrase()
+    selected_letters = []
 
-    display_phrase(phrase, ["m"])
+    while is_playing:
+        placeholder = build_placeholder(phrase, selected_letters)
 
-# Inside Dunder Main:
-## Create an instance of your Game class
-## Start your game by calling the instance method that starts the game loop
+        print(placeholder)
+
+        selected_letter = input("Guess a letter: ")
+
+        selected_letters.append(selected_letter.lower())
+
+
 if __name__ == "__main__":
     main()
