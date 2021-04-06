653c03d5183f3a30b4abee6918d68baf2f6c9785
Thomas Dimnet <thomas.dimnet@gmail.com>
2021-04-06 17:22:25 +0200

    Starts from functionnal programming:
    - Pick up random phrase
    - Display placeholder instead of phrase
    - Check if a letter is present or not

--- app.py
+++ app.py
@@ -1,11 +1,42 @@
-# Import your Game class
-from phrase_hunter.game import Game
+import random
 
-# Create your Dunder Main statement.
+PHRASES = [
+    "May the Force be with you.",
+    "Elementary, my dear Watson.",
+    "My mama always said life was like a box of chocolates.",
+    "My precious.",
+    "Bond. James Bond."
+]
+
+
+def display_phrase(phrase, test_letter):
+    print(list(phrase))
+    print(test_letter)
+
+    placeholder = []
+
+    for letter in phrase:
+        if letter.isalpha():
+            if letter.lower() == test_letter.lower():
+                placeholder.append(letter)
+            else:
+                placeholder.append("_")
+        else:
+            placeholder.append(letter)
+
+    print("====")
+    print("".join(placeholder))
+    print("====")
+
+
+def choose_random_phrase():
+    return random.choice(PHRASES)
+
+
 def main():
-    game = Game()
+    phrase = choose_random_phrase()
 
-    game.start()
+    display_phrase(phrase, ["m"])
 
 # Inside Dunder Main:
 ## Create an instance of your Game class
